4ea6a00c65e85ba00ea3f35e663534ee9b4b5ad5
Jhoão <jhpdrnsantos2@gmail.com>
2025-08-06 22:32:39 -0300

    feat: configuração e tipagem do zod

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import fastify from 'fastify';
 import { fastifyCors } from '@fastify/cors';
+import { validatorCompiler, serializerCompiler } from 'fastify-type-provider-zod';
 import { fastifySwagger } from '@fastify/swagger';
-import { fastifySwaggerUi } from '@fastify/swagger-ui';
+import fastifySwaggerUi from '@fastify/swagger-ui';
 
 const app = fastify();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
 
 app.register(fastifySwagger, {
     openapi: {
